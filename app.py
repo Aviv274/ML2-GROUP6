df9d7d7343606432f42ee60072359336b735e929
Aviv274 <aviv275@student.ie.edu>
2025-04-04 10:28:36 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,69 +6,149 @@ import datetime
 from agent import Agent
 import json
 import re
+import base64
 
 
 # Load environment variables
 load_dotenv()
 
+st.set_page_config(page_title="Trip Genie", layout="wide")
+
 # Constants
 CURRENT_YEAR = datetime.datetime.now().year
-
-# App config
-st.set_page_config(page_title="AI Travel Assistant", layout="wide")
-st.title("🌍 AI Travel Agent")
-st.markdown("""
-    <style>
-        .block-container {
-            padding-top: 2rem;
-        }
-        .card {
-            border-radius: 20px;
-            padding: 1.5rem;
-            background-color: #e3f2f9;
-            margin-bottom: 1rem;
-        }
-        .day-bubble {
-            border-radius: 25px;
-            padding: 1.5rem;
-            background-color: #f0fbff;
-            margin-bottom: 2rem;
-            border: 1px solid #cce5f6;
-        }
-        .day-header {
-            background-color: #eaf7ff;
-            padding: 0.5rem 1rem;
-            border-radius: 15px;
-            font-size: 1.2rem;
-            font-weight: bold;
-            display: inline-block;
-            margin-bottom: 1rem;
-        }
-        .activity {
-            margin-bottom: 0.5rem;
-            font-size: 1rem;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-
-# Instantiate the agent
-agent = Agent()
 response =""
 
-# Sidebar preferences form
-with st.sidebar:
-    st.header("📂 Travel Preferences")
-    origin = st.text_input("From where would you like to travel?")
-    destination = st.text_input("Where would you like to travel?")
-    start_date = st.date_input("Start date", datetime.date.today())
-    end_date = st.date_input("End date", datetime.date.today())
-    budget = st.selectbox("What is your budget level?", ["Low", "Medium", "High"])
-    interests = st.text_area("Main interests (e.g., food, museums, nature, nightlife)")
-    avoid = st.text_input("Anything to avoid?")
-    adult = st.number_input("Number of adult", min_value=0, value=1)
-    children = st.number_input("Number of children", min_value=0, value=0)
-    if st.button("🧽 Generate Trip Plan"):
+
+#Button state
+if "start_clicked" not in st.session_state:
+    st.session_state.start_clicked = False
+
+# Background Image function
+def get_base64_image(path):
+    with open(path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# Show background image until user clicks in the start button
+if not st.session_state.start_clicked:
+    background_base64 = get_base64_image("images/Trip_Genie.png")
+
+    page_bg = f"""
+    <style>
+    [data-testid="stApp"] {{
+        background-image: url("data:image/png;base64,{background_base64}");
+        background-size: cover;
+        background-position: center;
+    }}
+    [data-testid="stHeader"] {{
+        background: rgba(0,0,0,0);
+    }}
+    </style>
+    """
+    st.markdown(page_bg, unsafe_allow_html=True)
+
+    # Centered button
+    st.markdown("<br><br><br><br>", unsafe_allow_html=True)
+    st.markdown("##")
+    st.markdown("##")
+    st.markdown("##")
+    st.markdown("##")
+    st.markdown("##")
+    
+
+    col1, col2, col3 = st.columns([2, 2, 0.6])
+    with col2:
+        if st.button("Start Planning ✈️", key="start_button"):
+            st.session_state.start_clicked = True
+
+        
+    
+else:
+
+    # Instantiate the agent
+    agent = Agent()
+    response = ""
+
+    if "show_form" not in st.session_state:
+        st.session_state.show_form = True
+
+    page_bg = f"""
+    <style>
+    [data-testid="stApp"] {{
+        background-image: url("https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dHJhdmVsfGVufDB8fDB8fHww");
+        background-size: cover;
+        background-position: center;
+        color : black;
+        text-align: center;
+        border-radius : 15px
+    }}
+    [data-testid="stHeader"] {{
+        background: rgba(0,0,0,0);
+    }}
+    </style>
+    """
+    st.markdown(page_bg, unsafe_allow_html=True)
+
+
+
+
+    st.title("Plan Your Dream Trip")
+
+    st.markdown("")
+
+    with st.expander("✏️ Trip Preferences", expanded=st.session_state.show_form):
+
+        expander_bg = f"""
+        <style>
+        [data-testid="stExpander"] {{
+            background-color : white;
+            text-align: center;
+            border-radius : 15px
+        }}
+        
+        </style>
+        """
+        st.markdown(expander_bg, unsafe_allow_html=True)
+
+        st.markdown("")
+
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            origin = st.text_input("Origin 🏠")
+
+        with col2:
+            destination = st.text_input("Destination 🌆")
+
+        with col3:
+            start_date = st.date_input("Start date 📅", datetime.date.today())
+
+        with col4:
+            end_date = st.date_input("End date 📅", datetime.date.today())
+
+        
+        col5, col6, col7 = st.columns(3)
+
+        with col5:
+            adult = st.number_input("Number of adults 🧑 ", min_value=0, value=1)
+
+        with col6:
+            children = st.number_input("Number of children 👶 ", min_value=0, value=0)
+
+        with col7:
+            budget = st.selectbox("Budget Level 💰", ["Low", "Medium", "High"])
+
+        col8, col9 = st.columns(2)
+
+        with col8:
+            interests = st.text_area("Main interests ✨ (e.g., food, museums, nature, nightlife)")
+
+        with col9:
+            avoid = st.text_area("Anything to avoid? 🚫")
+
+        st.markdown("</div>", unsafe_allow_html=True)
+    
+
+    if st.button("✈️ Generate Itinerary",use_container_width=True):
         user_message = f"""
 Create a personalized itinerary.
 origin: {origin}
@@ -89,6 +169,8 @@ adult: {adult}
         st.session_state.adult = adult
         st.session_state.chat_history = [HumanMessage(content=user_message)]
         st.rerun()
+
+        #st.session_state.show_form = False
 
 
 
@@ -121,32 +203,115 @@ if "user_prompt" in st.session_state:
             st.warning("Please enter a valid message before generating the trip plan.")
 
 if response:
-    st.markdown(f"### {response['general']}")
-
-    hcol, fcol = st.columns(2)
-    with hcol:
-        st.markdown(f"""
-        <div class='card'>
-        <h4>🏨 Hotel</h4>
-        <p><strong>{response['hotel']['name']}</strong><br>
-        {response['hotel']['price_per_night']} per night<br>
-        ⭐ {response['hotel']['rating']}</p>
-        <a href="{response['hotel']['link']}" target="_blank">Hotel Website</a>
+    st.markdown("###")
+   
+    st.markdown(f"""
+        <div style="
+            background-color: rgba(255, 255, 255, 0.60);
+            padding: 2rem;
+            border-radius: 12px;
+            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+            text-align: center;
+        ">
+            <h3 style="margin: 0;">{response['general']}</h3>
         </div>
+    """, unsafe_allow_html=True)
+    
+    st.markdown("###")
+
+    hcol,space, fcol, space2, gcol = st.columns([2,0.1,2,0.1,2])
+    with hcol:
+        hotel = response.get("hotel", {})
+        link = hotel.get("link")
+
+        if not link and hotel.get("name"):
+           link = f"https://www.google.com/search?q={hotel['name'].replace(' ', '+')}+booking"
+
+        st.markdown(f"""
+            <div class='card' style="
+                background-color: rgba(255, 255, 255, 0.80);
+                border-radius: 15px;
+                padding: 1em;
+                text-align: center;
+                min-height: 230px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            ">
+                <div>
+                    <h4>🏨 Hotel </h4>
+                    <p><strong>{response['hotel']['name']}</strong><br>
+                    {response['hotel']['price_per_night']} per night<br>
+                    ⭐ {response['hotel']['rating']}</p>
+                </div>
+                <a href="{response['hotel']['link']}" target="_blank">Hotel Website</a>
+            </div>
         """, unsafe_allow_html=True)
 
     with fcol:
-        st.markdown(f"""
-        <div class='card'>
-        <h4>✈️ Flight</h4>
-        <p><strong>{response['flight']['airline']}</strong><br>
-        {response['flight']['departure_time']} → {response['flight']['arrival_time']}<br>
-        {response['flight']['departure_airport']} → {response['flight']['arrival_airport']}<br>
-        💰 {response['flight']['price']}</p>
-        </div>
-        """, unsafe_allow_html=True)
+        flight = response.get("flight", {})
 
-    st.markdown("### 🗓️ Itinerary")
+        if "outbound" in flight:
+            outbound = flight["outbound"]
+            airline = outbound.get('airline', 'N/A')
+            departure_time = outbound.get('departure_time', 'N/A')
+            arrival_time = outbound.get('arrival_time', 'N/A')
+            departure_airport = outbound.get('departure_airport', 'N/A')
+            arrival_airport = outbound.get('arrival_airport', 'N/A')
+            price = outbound.get('price', 'N/A')
+        
+            st.markdown(f"""
+    <div class='card' style="
+        background-color: rgba(255, 255, 255, 0.85);
+        padding: 1.5rem;
+        border-radius: 12px;
+        min-height: 230px;
+        box-shadow: 0 4px 12px rgba(0,0,0,0.1);
+        margin-bottom: 1.5rem;">
+        <h4>✈️ Outbound Flight</h4>
+        <p>
+            <strong>{airline}</strong><br>
+            {departure_time} → {arrival_time}<br>
+            {departure_airport} → {arrival_airport}<br>
+            💰 {price}<br>
+        </p>
+    </div>
+    """, unsafe_allow_html=True)
+
+    with gcol:
+        flight = response.get("flight", {})
+        
+        if "return" in flight:
+            returning = flight["return"]
+            airline = returning.get('airline', 'N/A')
+            departure_time = returning.get('departure_time', 'N/A')
+            arrival_time = returning.get('arrival_time', 'N/A')
+            departure_airport = returning.get('departure_airport', 'N/A')
+            arrival_airport = returning.get('arrival_airport', 'N/A')
+            price = returning.get('price', 'N/A')
+     
+  
+            st.markdown(f"""
+    <div class='card' style="
+        background-color: rgba(255, 255, 255, 0.85);
+        padding: 1.5rem;
+        min-height: 230px;
+        border-radius: 12px;
+        box-shadow: 0 4px 12px rgba(0,0,0,0.1);">
+        <h4>🔁 Return Flight</h4>
+        <p>
+            <strong>{airline}</strong><br>
+            {departure_time} → {arrival_time}<br>
+            {departure_airport} → {arrival_airport}<br>
+            💰 {price}<br>
+        </p>
+    </div>
+    """, unsafe_allow_html=True)
+        
+
+    st.markdown("###")
+
+    st.markdown("<h3 style='text-align: center;'>🗓️ Itinerary</h3>", unsafe_allow_html=True)
     day_map = {
         "visit": "🏛️",
         "explore": "🧭",
@@ -167,11 +332,19 @@ if response:
         for day_dict in plan_list:
             for raw_day, activities in day_dict.items():
                 day_label = raw_day.replace("day", "Day ").capitalize()
-                html = f"<div class='day-bubble'><div class='day-header'>📅 {day_label}</div>"
+                html = f"""
+            <div class='day-bubble' style="
+                background-color: rgba(255, 255, 255, 0.80);
+                font-weight: bold;
+                text-align: center;
+                border-radius: 8px;
+                padding: 1em;
+                margin-bottom: 1.5em;
+            ">
+                <div class='day-header'>📅 {day_label}</div>
+            """
                 for item in activities:
                     icon = day_map.get(item['type'].lower(), "📍")
                     html += f"<p class='activity'><strong>{item['time']}</strong> — {icon} <strong>{item['type']}</strong>: {item['description']}</p>"
                 html += "</div>"
                 st.markdown(html, unsafe_allow_html=True)
-
-
